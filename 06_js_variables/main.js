5522501265b20c5347e5c7447f6245b30d504807
Lukas Müller <Lukas.V.Mueller@student.uibk.ac.at>
2020-03-25 15:21:49 +0100

    String Interpolation

--- 06_js_variables/main.js
+++ 06_js_variables/main.js
@@ -1,2 +1,3 @@
-var favoriteAnimal = 'Mäusebussard'
-console.log('My favorite animal: ' + favoriteAnimal);
+var myName = 'Kaschperl'
+var myCity = 'Passau'
+console.log(`My name is ${myName}. My favorite city is ${myCity}.`)
