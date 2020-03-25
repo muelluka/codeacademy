2149ef97a86d84fa744ece9d6b88b0a48bf0bd6e
Lukas Müller <Lukas.V.Mueller@student.uibk.ac.at>
2020-03-25 15:59:02 +0100

    typeof operator

--- 06_js_variables/main.js
+++ 06_js_variables/main.js
@@ -1,3 +1,4 @@
-var myName = 'Kaschperl'
-var myCity = 'Passau'
-console.log(`My name is ${myName}. My favorite city is ${myCity}.`)
+let newVariable = 'Playing around with typeof.';
+console.log(typeof newVariable)
+newVariable = 1
+console.log(typeof newVariable)
